0537508c110559dc3547692f5b42571e410fd092
Adrian <adrian@MBP.local>
2017-09-28 19:16:25 +0800

    Initial commit

---
+++ app/src/main/java/adrianjayson/com/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,113 @@
+package adrianjayson.com.interactivestory.ui;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Stack;
+
+import adrianjayson.com.interactivestory.R;
+import adrianjayson.com.interactivestory.model.Page;
+import adrianjayson.com.interactivestory.model.Story;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static String TAG = StoryActivity.class.getSimpleName();
+
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
+    private String name;
+    private Stack<Integer> pageStack = new Stack<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        storyImageView = (ImageView) findViewById(R.id.storyImageView);
+        storyTextView = (TextView)findViewById(R.id.storyTextView);
+        choice1Button = (Button)findViewById(R.id.choice1Button);
+        choice2Button = (Button)findViewById(R.id.choice2Button);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra(getString(R.string.key_name));
+        Log.d(TAG, name );
+
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+        pageStack.push(pageNumber);
+
+        final Page page = story.getPage(pageNumber);
+
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
+
+        String pageText = getString(page.getTextId());
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+
+        if (page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.playAgainButtonText);
+            choice2Button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                    finish();
+                    pageStack.clear();
+                    loadPage(0);
+                }
+            });
+        } else {
+            loadButtons(page);
+        }
+
+    }
+
+    private void loadButtons(final Page page) {
+        choice1Button.setVisibility(View.VISIBLE);
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        choice2Button.setVisibility(View.VISIBLE);
+        choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        pageStack.pop();
+        if (pageStack.isEmpty()) {
+            super.onBackPressed();
+        } else {
+            loadPage(pageStack.pop());
+        }
+
+    }
+}
